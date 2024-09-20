0a6b10a044da4a0825a3f28d790762452c2ef7bd
MaverickUst <mauri.ust@gmail.com>
2024-09-20 12:54:10 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,32 @@
+let y = 0
+let x = 0
 basic.clearScreen()
+let cicli = 5
 basic.forever(function () {
-    basic.showString("Hello!")
+    x = 0
+    for (let index = 0; index < cicli; index++) {
+        control.waitMicros(200000)
+        led.plot(x, 0)
+        x = x + 1
+    }
+    x = 0
+    for (let index = 0; index < cicli; index++) {
+        control.waitMicros(200000)
+        led.unplot(x, 0)
+        x = x + 1
+    }
+    x = 0
+    y = 0
+    for (let index = 0; index < cicli; index++) {
+        control.waitMicros(200000)
+        led.plot(4, y)
+        y = y + 1
+    }
+    x = 0
+    y = 0
+    for (let index = 0; index < cicli; index++) {
+        control.waitMicros(200000)
+        led.unplot(4, y)
+        y = y + 1
+    }
 })
